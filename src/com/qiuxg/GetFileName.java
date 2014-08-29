7bf1796b78df726b3109ace85be207d049fb2110
qiu1157 <qiuxiangu@gmail.com>
2014-08-29 16:23:11 +0800

    Change-Id: I0000000000000000000000000000000000000000 Signed-off-by: qiu1157 <qiuxiangu@gmail.com> 增加文件名过滤，只有excel文件才能搜索到

--- src/com/qiuxg/GetFileName.java
+++ src/com/qiuxg/GetFileName.java
@@ -11,24 +11,14 @@ public class GetFileName {
 	 */
 	List<File> ll = new ArrayList<File>();
 	public List<File> getFileName(File file) {
-//		File file = new File(dir);
-//		//List<File> l = new ArrayList<File>();
-//		if(!file.exists()||!file.isDirectory()) {
-//			System.out.println("目录不正确");
-//			return null;
-//		}
-//		File[] files = file.listFiles();
-//		for(File file2:files) {
-//			if(file2.isFile()) {
-//				l.add(file2);
-//			}
-//		}
 		File[] files = file.listFiles();
 		for(File file2 : files) {
 			if(file2.isDirectory()) {
 				getFileName(file2);
 			}else {
-				ll.add(file2);  
+				if(file2.getName().endsWith("xlsx")) {
+					ll.add(file2);
+				}
 			}
 		}
 		return ll;
@@ -36,7 +26,7 @@ public class GetFileName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GetFileName gfn = new GetFileName();
-		File file = new File("D:\\个人知识管理\\1_项目工作\\10_长沙银行\\01_财富管理系统\\10_相关资料\\中信银行mapping\\ACRM");
+		File file = new File("d:\\个人知识管理\\1_项目工作\\10_长沙银行\\01_财富管理系统\\11_mapping\\");
 		List<File> l = gfn.getFileName(file);
 		for(File file2 : l) {
 			System.out.println(file2.getName());
